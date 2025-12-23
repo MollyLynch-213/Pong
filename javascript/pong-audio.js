953057ebf9d7c37d95d040819e8dacca68c07401
Molly <moelly2006@gmail.com>
2025-12-23 16:18:15 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -55,12 +55,12 @@ soundArray.push(scoreSound);
 export var ambientSound = new soundFile("Running.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
-ambientSound.player.volume.value = 15; //turn down volume
+ambientSound.player.volume.value = 30; //turn down volume
 
 export var adventureMusic = new soundFile("Running.mp3");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
-adventureMusic.player.volume.value = 15;
+adventureMusic.player.volume.value = 30;
 
 export var villageMusic = new soundFile("silence.mp3");
 soundArray.push(villageMusic);
